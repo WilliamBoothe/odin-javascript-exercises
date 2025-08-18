abeaa849695dfa0d00a607740c4729102131e89f
William Boothe <89033570+WilliamBoothe@users.noreply.github.com>
2025-08-18 22:06:50 +0000

    06_repeatString complete

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  finalWord = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (i = 0; i < num; i++) {
+    finalWord += string;
+  }
 
+  return finalWord;
 };
 
 // Do not edit below this line
