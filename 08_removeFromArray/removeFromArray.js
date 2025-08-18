ad5112e665605a6791969fbf52a0d38cb23daf46
William Boothe <89033570+WilliamBoothe@users.noreply.github.com>
2025-08-18 22:25:00 +0000

    08_removeFromArray complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayToSearch, ...itemsToRemove) {
+  for (let i = 0; i < arrayToSearch.length; i++) {
+    if (itemsToRemove.includes(arrayToSearch[i])) {
+      arrayToSearch.splice(i, 1);
+      i--;
+    }
+  }
+
+  return arrayToSearch;
 };
 
 // Do not edit below this line
